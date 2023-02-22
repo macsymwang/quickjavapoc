8e0dea9a20bd1f46b6f0c57be8b6b7aab05dbdb7
macsymwang <56002160+macsymwang@users.noreply.github.com>
2023-02-22 17:20:23 +0000

    String to Date

--- src/main/java/com/telus/mediationcloud/quick/poc/FileName.java
+++ src/main/java/com/telus/mediationcloud/quick/poc/FileName.java
@@ -1,5 +1,11 @@
 package com.telus.mediationcloud.quick.poc;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,6 +20,14 @@ public class FileName implements CommandLineRunner {
     public void run(String... args) throws Exception {
         String fileFullName = "gs://ccs_data_tdr_process_np-c0f674/db_load/SUODS_FWHSIA_ID_20230203_1_meta_agentmetaname-CloudDBExtractionPublisher.json";
         log.info("File name:" + FilenameUtils.getName(fileFullName));
+
+        String endTime = "65535-12-31T23:59:59.999999Z";
+        Date endTimeDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").parse(endTime);
+        System.out.println("endTimeDate:" + endTimeDate);
+
+        endTime = "2023-02-16T16:14:04.000000Z";
+        endTimeDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").parse(endTime);
+        System.out.println("endTimeDate:" + endTimeDate);
     }
 
 }
