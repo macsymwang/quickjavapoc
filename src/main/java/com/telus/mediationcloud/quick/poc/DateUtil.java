46486a132772b852604e11caec39defc06492b5b
macsymwang <56002160+macsymwang@users.noreply.github.com>
2023-06-28 17:17:39 +0000

    update

--- src/main/java/com/telus/mediationcloud/quick/poc/DateUtil.java
+++ src/main/java/com/telus/mediationcloud/quick/poc/DateUtil.java
@@ -6,6 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class DateUtil implements CommandLineRunner {
@@ -16,5 +18,10 @@ public class DateUtil implements CommandLineRunner {
         String sDate1 = "20230610070000";
         Date date1 = new SimpleDateFormat("yyyyMMddHHmmss").parse(sDate1);
         System.out.println(sDate1 + "\t" + date1);
+
+        String sDate2 = "2023-06-10T07:00:00";
+        LocalDateTime date2 = LocalDateTime.parse(sDate2);
+        System.out.println(sDate2 + "\t" + date2.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
     }
 }
