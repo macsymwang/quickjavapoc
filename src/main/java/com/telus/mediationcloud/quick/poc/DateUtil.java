cba7ecd51b2b9fec80999818e0de8602a33cd88b
macsymwang <macsym.wang@telus.com>
2023-10-20 16:17:47 -0400

    Update DateUtil.java

--- src/main/java/com/telus/mediationcloud/quick/poc/DateUtil.java
+++ src/main/java/com/telus/mediationcloud/quick/poc/DateUtil.java
@@ -4,12 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-// @Component
+@Component
 public class DateUtil implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(DateUtil.class);
 
@@ -22,6 +24,15 @@ public class DateUtil implements CommandLineRunner {
         String sDate2 = "2023-06-10T07:00:00";
         LocalDateTime date2 = LocalDateTime.parse(sDate2);
         System.out.println(sDate2 + "\t" + date2.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        String sDate3 = "2023-12-26T20:47:47Z";
+        Date date3 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(sDate3);
+        TimeZone tz = TimeZone.getTimeZone("America/Toronto");
+        System.out.println(sDate3 + " in day light saving:\t" + tz.inDaylightTime(date3));
+
+        sDate3 = "2023-08-26T20:47:47Z";
+        date3 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(sDate3);
+        System.out.println(sDate3 + " in day light saving:\t" + tz.inDaylightTime(date3));
 
     }
 }
